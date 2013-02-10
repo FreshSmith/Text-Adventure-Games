92a644e10b23abe7425b9da7e0fba6809eb78133
gmeben <gmeben@gmail.com>
2013-02-09 20:50:03 -0500

    Added initial state
    + added first dialogue feedback
    + added LOOK AROUND option to controls scheme

--- bcca/js/cosby.js
+++ bcca/js/cosby.js
@@ -1,1 +1,12 @@
-
+jQuery(document).ready(function($){
+    function bcca(){
+        // This contains all of the game dialogue
+        var d = [
+            "<p>You are at the entrance of the circus.</p>"
+        ];
+        
+        // This is where game dialogue gets sent to the player
+        $('.feedback').append(d[0]);
+    }
+    bcca();
+});
